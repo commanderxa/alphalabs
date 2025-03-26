e4a6284cb9c521b249f9d60d9ed036b9a4158757
commanderxa <art.ostrikov@gmail.com>
2025-03-27 02:24:03 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const isProd = process.env.NODE_ENV === 'production';
-console.log(process.env.NODE_ENV === 'production');
-console.log(process.env.NODE_ENV);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
