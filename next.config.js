bc777ba42072eac74453e956a5b686c590df765d
commanderxa <art.ostrikov@gmail.com>
2025-03-27 02:05:19 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production';
+console.log(process.env.NODE_ENV === 'production');
+console.log(process.env.NODE_ENV);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
     basePath: isProd ? "/alphalabs" : "",
-    assetPrefix: isProd ? "/alphalabs/" : "",
+    // assetPrefix: isProd ? "/alphalabs/" : "",
     images: {
         unoptimized: true,
         // loader: 'default',
