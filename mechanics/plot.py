29ccc8693921da382d7c47b91aac3ea32b61d9aa
commanderxa <art.ostrikov@gmail.com>
2025-03-26 14:46:54 +0500

    Clean slate for website

--- mechanics/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-# set nice theme for plots
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.serif"] = ["Times New Roman"] + plt.rcParams["font.serif"]
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams["axes.ymargin"] = 0.0
-plt.rcParams["axes.xmargin"] = 0.0
-plt.rcParams['savefig.pad_inches'] = 0.01
--- output/README.md
+++
@@ -1,3 +0,0 @@
-# Media Output Directory
-
-This directory will have all of the media outputs produces by the code.
